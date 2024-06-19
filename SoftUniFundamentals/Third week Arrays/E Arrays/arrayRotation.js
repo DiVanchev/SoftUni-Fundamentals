dea2e9ce5371bc967d20e252dc0559d125ef799f
Dimitar Vachev <d.vachev2002@gmail.com>
2024-06-19 18:39:02 +0300

    yep

--- SoftUniFundamentals/Third week Arrays/E Arrays/arrayRotation.js
+++ SoftUniFundamentals/Third week Arrays/E Arrays/arrayRotation.js
@@ -1,18 +1,9 @@
 function arrayRotation(arr, rotations) {
-<<<<<<< HEAD
     for (let i = 0; i < rotations; i++) {
       let fElem = arr.shift();
       arr.push(fElem);
     }
     console.log(arr.join(' '));
   }
-  arrayRotation([51, 47, 32, 61, 21], 2);
-  =======
-  for (let i = 0; i < rotations; i++) {
-    let fElem = arr.shift();
-    arr.push(fElem);
-  }
-  console.log(arr.join(' '));
-}
 arrayRotation([51, 47, 32, 61, 21], 2);
->>>>>>> d1c870a7a35840a9ebcfd30b7f8cc0dc5086b28b
+
