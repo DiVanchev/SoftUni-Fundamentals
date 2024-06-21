11205c9fa38a443375b199c52081e670637bce14
Dimitar Vachev <d.vachev2002@gmail.com>
2024-06-22 01:15:40 +0300

    update

---
+++ SoftUniFundamentals/Fifth week Advanced arrays/arrayManipulation.js
@@ -0,0 +1,16 @@
+function arrayManipulation (commands){
+        let arr = commands
+            .shift()
+            .split()
+            .map(Number);
+
+        for (let i = 0; i < array.length; i++){
+            
+        }
+}
+arrayManipulation(['4 19 2 53 6 43',
+'Add 3',
+'Remove 2',
+'RemoveAt 1',
+'Insert 8 3']
+);
--- SoftUniFundamentals/Fifth week Advanced arrays/listOfProducts.js
+++ SoftUniFundamentals/Fifth week Advanced arrays/listOfProducts.js
@@ -1,3 +1,7 @@
-function listOfProducts (){
-    
+function listOfProducts (arr){
+    let sortedArr = arr.sort();
+    for (let i = 0; i < sortedArr.length; i++){
+        console.log(`${i + 1}.${sortedArr[i]}`);
+    }
 }
+listOfProducts(['Potatoes', 'Tomatoes', 'Onions', 'Apples']);
