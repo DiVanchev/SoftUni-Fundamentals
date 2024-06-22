e32f5c61434ee88cc9e0013a7f7610f5bfde0254
Dimitar Vachev <d.vachev2002@gmail.com>
2024-06-22 21:00:54 +0300

    update

--- SoftUniFundamentals/Fifth week Advanced arrays/E Advanced arrays/bombNumbers.js
+++ SoftUniFundamentals/Fifth week Advanced arrays/E Advanced arrays/bombNumbers.js
@@ -1,9 +1,6 @@
-function bombNumbers (arr1, arr2) {
-    
+function bombNumbers (arrSequence, arrBomb) {
+
 }
 bombNumbers([1, 2, 2, 4, 2, 2, 2, 9],
     [4, 2]
     );
-bombNumbers([1, 4, 4, 2, 8, 9, 1],
-    [9, 3]
-    );  
