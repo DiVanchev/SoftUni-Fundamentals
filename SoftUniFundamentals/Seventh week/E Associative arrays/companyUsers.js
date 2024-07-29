45115a1f763ebe1ad5108b793ec5c294214e06a9
Dimitar Vachev <d.vachev2002@gmail.com>
2024-07-29 17:09:08 +0300

    update

--- SoftUniFundamentals/Seventh week/E Associative arrays/companyUsers.js
+++ SoftUniFundamentals/Seventh week/E Associative arrays/companyUsers.js
@@ -1,5 +1,28 @@
 function companyUsers(arr) {
-    
+  const companyEmployees = {};
+
+  for (const employeeData of arr) {
+    const [company, employeeId] = employeeData.split(" -> ");
+
+    if (company in companyEmployees) {
+      
+      if (!companyEmployees[company].includes(employeeId)) {
+        companyEmployees[company].push(employeeId);
+      }
+    } else {
+      companyEmployees[company] = [employeeId];
+    }
+  }
+  
+  const companyEntries = Object.entries(companyEmployees).sort((a, b) => a[0].localeCompare(b[0]));
+
+  for (const [company, employeeArr] of companyEntries) {
+      console.log(`${company}`);      
+      for (const employeeId of employeeArr) {
+          console.log(`-- ${employeeId}`);
+      }
+  }
+
 }
 companyUsers([
   "SoftUni -> AA12345",
@@ -7,10 +30,10 @@ companyUsers([
   "Microsoft -> CC12345",
   "HP -> BB12345",
 ]);
-companyUsers([
-  "SoftUni -> AA12345",
-  "SoftUni -> CC12344",
-  "Lenovo -> XX23456",
-  "SoftUni -> AA12345",
-  "Movement -> DD11111",
-]);
+//companyUsers([
+//  "SoftUni -> AA12345",
+//  "SoftUni -> CC12344",
+//  "Lenovo -> XX23456",
+//  "SoftUni -> AA12345",
+//  "Movement -> DD11111",
+//]);
