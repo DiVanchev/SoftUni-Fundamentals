5bb0f2965130c61c373ca161323ea6038f203d41
Dimitar Vachev <d.vachev2002@gmail.com>
2024-08-01 16:19:12 +0300

    update

--- SoftUniFundamentals/Ninth week Regular Expressions/L Regular Expressions/matchFullName.js
+++ SoftUniFundamentals/Ninth week Regular Expressions/L Regular Expressions/matchFullName.js
@@ -1,3 +1,19 @@
-function matchFullName() {
+function matchFullName(names) {
+  let pattern = /\b[A-Z][a-z]+[ ][A-Z][a-z]+\b/g;
 
+
+  let validNames = [];
+  let validName;
+
+  while ((validName = pattern.exec(names)) !== null) {
+    validNames.push(validName[0]);
+  }
+
+  const validNamesJoined = validNames.join(" ");
+
+  console.log(validNamesJoined);
 }
+
+matchFullName(
+  "Ivan Ivanov, Ivan ivanov, ivan Ivanov, IVan Ivanov, Test Testov, Ivan	Ivanov"
+);
--- SoftUniFundamentals/Ninth week Regular Expressions/L Regular Expressions/matchPhoneNumber.js
+++ SoftUniFundamentals/Ninth week Regular Expressions/L Regular Expressions/matchPhoneNumber.js
@@ -1,3 +1,11 @@
-function matchPhoneNumber() {
-    
+function regExPhones(input) {
+  let validNames = [];
+  let pattern = /(?<!\d)[+]359([ -])2\1\d{3}\1\d{4}\b/g;
+  while ((validName = pattern.exec(input)) !== null) {
+    validNames.push(validName[0]);
+  }
+  console.log(validNames.join(", "));
 }
+regExPhones([
+  "+359 2 222 2222,359-2-222-2222, +359/2/222/2222, +359-2 222 2222 +359 2-222-2222, +359-2-222-222, +359-2-222-22222 +359-2-222-2222",
+]);
