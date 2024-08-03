84f4aaff38675da417decf6c596e426eeb703bab
Dimitar Vachev <d.vachev2002@gmail.com>
2024-08-03 14:29:05 +0300

    update

--- SoftUniFundamentals/Fifth week Advanced arrays/E Advanced arrays/bombNumbers.js
+++ SoftUniFundamentals/Fifth week Advanced arrays/E Advanced arrays/bombNumbers.js
@@ -1,6 +1,16 @@
-function bombNumbers (arrSequence, arrBomb) {
+function bombDetonate(sequence, bombInfo) {
+  const [bombNumber, power] = bombInfo;
 
+  let index;
+  while ((index = sequence.indexOf(bombNumber)) !== -1) {
+    let start = Math.max(0, index - power);
+    let end = Math.min(sequence.length - 1, index + power);
+
+    sequence.splice(start, end - start + 1);
+  }
+
+  let result = sequence.reduce((acc, num) => acc + num, 0);
+
+  console.log(result);
 }
-bombNumbers([1, 2, 2, 4, 2, 2, 2, 9],
-    [4, 2]
-    );
+bombDetonate([1, 2, 2, 4, 2, 2, 2, 9], [4, 2]);
---
+++ SoftUniFundamentals/Fifth week Advanced arrays/E Advanced arrays/searchForANumber.js
@@ -0,0 +1,17 @@
+function searchForANumber(mainArr, cringeArr) {
+  const [take, deleteIt, searchFor] = cringeArr;
+
+  let newArr = mainArr.slice(0, take);
+  newArr.splice(0, deleteIt)
+  let numCounter = 0;
+  console.log(newArr);
+  
+  for (const numbers of newArr) {
+    if (numbers === searchFor) {
+      numCounter++;
+    }
+  }
+  console.log(`Number ${searchFor} occurs ${numCounter} times.`);
+}
+searchForANumber([5, 2, 3, 4, 1, 6], [5, 2, 3]);
+searchForANumber([7, 5, 5, 8, 2, 7], [3, 1, 5]);
