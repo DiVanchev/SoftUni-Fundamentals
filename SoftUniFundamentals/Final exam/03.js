89ae085fe4480f27ad3b72fbb8537aef55ea9ca7
Dimitar Vachev <d.vachev2002@gmail.com>
2024-08-04 14:31:31 +0300

    update

--- SoftUniFundamentals/Final exam/03.js
+++ SoftUniFundamentals/Final exam/03.js
@@ -1,23 +1,63 @@
 function solve(arr) {
-    let socialData = {};
-    let command = arr.shift();
+  let socialData = {};
+  let command = arr.shift();
 
-    while(command !== 'Log out') {
-        
+  let followerCounter = 0;
+
+  while (command !== "Log out") {
+    let tokens = command.split(":");
+    let action = tokens.shift();
+
+    if (action === "New follower") {
+      let name = tokens.shift();
+
+      if (!socialData[name]) {
+        socialData[name] = { likes: 0, comments: 0 };
+        followerCounter++;
+      }
+    } else if (action === "Like") {
+      let name = tokens.shift();
+      let likesCounter = Number(tokens.shift());
+      if (socialData[name]) {
+        socialData[name].likes += likesCounter;
+      } else {
+        socialData[name] = { likes: likesCounter, comments: 0 };
+        followerCounter++;
+      }
+    } else if (action === "Comment") {
+      let name = tokens.shift();
+
+      if (socialData[name]) {
+        socialData[name].comments += 1;
+      } else {
+        socialData[name] = { likes: 0, comments: 1 };
+        followerCounter++;
+      }
+    } else if (action === "Blocked") {
+      let name = tokens.shift();
+      if (socialData[name]) {
+        delete socialData[name];
+        followerCounter--;
+      } else {
+        console.log(`${name} doesn't exist.`);
+      }
     }
+
+    command = arr.shift();
+  }
+  console.log(`${followerCounter} followers`);
+
+  let socialEntries = Object.entries(socialData);
+
+  for (const [name, { likes, comments }] of socialEntries) {
+    console.log(`${name}: ${likes + comments}`);
+  }
 }
 solve([
-  "New follower: George",
-  "Like: George: 5",
-  "New follower: George",
-  "Log out",
-]);
-solve([
-  "Like: Katy: 3",
-  "Comment: Katy",
-  "New follower: Bob",
-  "Blocked: Bob",
+  "Blocked: Amy",
+  "Comment: Amy",
   "New follower: Amy",
-  "Like: Amy: 4",
+  "Like: Tom: 5",
+  "Like: Ellie: 5",
   "Log out",
 ]);
